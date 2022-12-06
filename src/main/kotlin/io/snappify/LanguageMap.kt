49c0db47da2df82ed8f32a403f7fc4b1375a5b09
Dominik Sumer <domi.sumer@gmail.com>
2022-12-06 16:16:16 +0100

    reverted plugin id change

--- src/main/kotlin/io/snappify/LanguageMap.kt
+++ src/main/kotlin/io/snappify/LanguageMap.kt
@@ -1,4 +1,4 @@
-package com.snappify
+package io.snappify
 
 object LanguageMap {
     private const val C = "c"
--- src/main/kotlin/io/snappify/OpenAction.kt
+++ src/main/kotlin/io/snappify/OpenAction.kt
@@ -1,4 +1,4 @@
-package com.snappify
+package io.snappify
 
 import com.intellij.ide.BrowserUtil
 import com.intellij.openapi.actionSystem.AnAction
